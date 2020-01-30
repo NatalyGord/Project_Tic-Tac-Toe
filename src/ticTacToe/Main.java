954dac9c49f974ef4315ee012b8e828ce1ea0e8a
Nataly <NatGord@rambler.ru>
2020-01-30 22:53:36 +0700

    создание таблички Х и 0

---
+++ src/ticTacToe/Main.java
@@ -0,0 +1,10 @@
+package ticTacToe;
+
+public class Main {
+
+    public static void main(String[] args){
+        System.out.println("X " + "0 " + "X ");
+        System.out.println("0 " + "X " + "X ");
+        System.out.println("X " + "0 " + "0 ");
+    }
+}
