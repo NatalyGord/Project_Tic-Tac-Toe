f7f81f0730975e9db7b9c33eeb754a66c85f166d
Nataly <NatGord@rambler.ru>
2020-01-30 23:47:55 +0700

    считывание символов и запись их в табличку

--- src/ticTacToe/Main.java
+++ src/ticTacToe/Main.java
@@ -1,17 +1,34 @@
 package ticTacToe;
-
+import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args){
-        System.out.print('X' + " ");
-        System.out.print('O' + " ");
-        System.out.println('X');
-        System.out.print('O' + " ");
-        System.out.print('X' + " ");
-        System.out.println('O');
-        System.out.print('X' + " ");
-        System.out.print('X' + " ");
-        System.out.println('O');
-
+        Scanner scanner = new Scanner(System.in);
+        char a1 = scanner.findInLine(".").charAt(0);
+        char a2 = scanner.findInLine(".").charAt(0);
+        char a3 = scanner.findInLine(".").charAt(0);
+        char b1 = scanner.findInLine(".").charAt(0);
+        char b2 = scanner.findInLine(".").charAt(0);
+        char b3 = scanner.findInLine(".").charAt(0);
+        char c1 = scanner.findInLine(".").charAt(0);
+        char c2 = scanner.findInLine(".").charAt(0);
+        char c3 = scanner.findInLine(".").charAt(0);
+        System.out.println("---------");
+        System.out.print('|' + " ");
+        System.out.print(a1 + " ");
+        System.out.print(a2 + " ");
+        System.out.print(a3 + " ");
+        System.out.println('|' + " ");
+        System.out.print('|' + " ");
+        System.out.print(b1 + " ");
+        System.out.print(b2 + " ");
+        System.out.print(b3 + " ");
+        System.out.println('|' + " ");
+        System.out.print('|' + " ");
+        System.out.print(c1 + " ");
+        System.out.print(c2 + " ");
+        System.out.print(c3 + " ");
+        System.out.println('|' + " ");
+        System.out.println("---------");
     }
 }
