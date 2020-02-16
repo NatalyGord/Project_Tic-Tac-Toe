7c1a9723eab3a73193d100a68e8a0027db7bc17d
Nataliya Gordiets <NatGord@rambler.ru>
2020-02-16 20:38:39 +0600

    Наброски

--- src/ticTacToe/Main.java
+++ src/ticTacToe/Main.java
@@ -31,5 +31,37 @@ public class Main {
         System.out.print(c3 + " ");
         System.out.println('|' + " ");
         System.out.println("---------");
+        int n = 0;
+        int nX = 0;
+        int nO = 0;
+        int i = 0;
+        for(i = 1; i < 10; i++){
+            char a = scanner.findInLine(".").charAt(0);
+            if(a == 'X'){
+                nX++;
+                //win X
+                //
+            }
+            if(a == 'O'){
+                nO++;
+                //win O
+            }
+            if(nX - nO > 1 || nX - nO < -1){  // прописать когда и х и о выигрышные
+                System.out.println("Impossible");
+            }
+            if(a == '_'){                     //это после проверки на выигрыш
+                n++;
+                if(n > 0){
+                    System.out.println("Game not finished");
+                }
+            }
+
+        }
+
+        if(a1 == a2 && a2 == a3 || b1 == b2 && b2 == b3 || c1 == c2 && c2 == c3 ||
+                a1 == b1 && b1 == c1 || a2 == b2 && b2 == c2 || a3 == b3 && b3 == c3 ||
+                a3 == b2 && b2 == c1 || a1 == b2 && b2 == c3){
+            System.out.println(a1 + " wins");
+        }
     }
 }
