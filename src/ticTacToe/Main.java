10de59335ed699047ec4b3df454fc265f0292367
Nataliya Gordiets <NatGord@rambler.ru>
2020-03-21 20:10:50 +0600

    небольшие правки по _

--- src/ticTacToe/Main.java
+++ src/ticTacToe/Main.java
@@ -40,8 +40,8 @@ public class Main {
         }
         System.out.println("---------");
         //System.out.println(nX + " - X");
-        //System.out.println(nO + " - O");
-        //System.out.println(n + " - _ ");
+       // System.out.println(nO + " - O");
+       // System.out.println(n + " - _ ");
 
         if (nX - nO > 1 || nX - nO < -1) {   //ошибка когда Х или О больше чем на 1
             System.out.println("Impossible");
@@ -50,29 +50,40 @@ public class Main {
             if ((arr[0][0] == arr[0][1] && arr[0][0] == arr[0][2])
                 || (arr[0][0] == arr[1][0] && arr[0][0] == arr[2][0])
                 || (arr[0][0] == arr[1][1] && arr[0][0] == arr[2][2])){
-                res++;
-                win = arr[0][0];
+
+                if (arr[0][0] == 'X' || arr[0][0] == 'O') {
+                    win = arr[0][0];
+                    res++;
+                }
             }
             if ((arr[1][0] == arr[1][1] && arr[1][0] == arr[1][2])
                 || (arr[0][1] == arr[1][1] && arr[0][1] == arr[2][1])){
-                res++;
-                win = arr[1][1];
+                if (arr[1][1] == 'X' || arr[1][1] == 'O') {
+                    win = arr[1][1];
+                    res++;
+                }
             }
             if (arr[2][0] == arr[2][1] && arr[2][0] == arr[2][2]) {
-                res++;
-                win = arr[2][0];
+                if (arr[2][0] == 'X' || arr[2][0] == 'O') {
+                    win = arr[2][0];
+                    res++;
+                }
             }
             if ((arr[0][2] == arr[1][2] && arr[0][2] == arr[2][2])
                 || (arr[0][2] == arr[1][1] && arr[0][2] == arr[2][0])){
-                res++;
-                win = arr[0][2];
+                if (arr[0][2] == 'X' || arr[0][2] == 'O') {
+                    win = arr[0][2];
+                    res++;
+                }
             }
             if(res >= 2){
                 System.out.println("Impossible");
             }
             else{
                 if(res == 1) {
-                    System.out.println(win + " wins");
+                    if (win == 'X' || win == 'O') {
+                        System.out.println(win + " wins");
+                    }
                 }
                 else{
                     if(res == 0){
