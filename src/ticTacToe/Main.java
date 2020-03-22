3569f4c6d63c820566bacd783410464362598bea
Nataliya Gordiets <NatGord@rambler.ru>
2020-03-22 21:01:18 +0600

    Делаем ход

--- src/ticTacToe/Main.java
+++ src/ticTacToe/Main.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int n = 0;
         int nX = 0;
@@ -13,13 +13,12 @@ public class Main {
         int res = 0; //считает количество выигрышей
         char win = '0';
         char arr[][] = new char[3][3];
-        for(i = 0; i < 3; i++){         //заполняем массив
-            for(j =0; j < 3; j++) {
+        for (i = 0; i < 3; i++) {         //заполняем массив
+            for (j = 0; j < 3; j++) {
                 arr[i][j] = scanner.findInLine(".").charAt(0);
-                if(arr[i][j] == 'X'){  //считает количество Х
+                if (arr[i][j] == 'X') {  //считает количество Х
                     nX++;
-                }
-                else {
+                } else {
                     if (arr[i][j] == 'O') { //считает количество О
                         nO++;
                     } else {
@@ -31,54 +30,76 @@ public class Main {
             }
         }
         System.out.println("---------");     //печатаем поле
-        for(i = 0; i < 3; i++){
+        for (i = 0; i < 3; i++) {
             System.out.print('|' + " ");
-            for(j =0; j < 3; j++) {
+            for (j = 0; j < 3; j++) {
                 System.out.print(arr[i][j] + " ");
             }
             System.out.println('|' + " ");
         }
         System.out.println("---------");
         //System.out.println(nX + " - X");
-       // System.out.println(nO + " - O");
-       // System.out.println(n + " - _ ");
-        System.out.println();
-        System.out.println("Enter the coordinates: ");
+        // System.out.println(nO + " - O");
+        // System.out.println(n + " - _ ");
+
+       // System.out.println();
+        System.out.print("Enter the coordinates: ");
         String a = scanner.next();
-        String b = scanner.next();
+        String b = scanner.next();;
         int aInt = 0;
         int bInt = 0;
+
         while(true) {
-            if (a.matches("[0-9]+") && b.matches("[0-9]+")) {     //проверка на число
-                if(a.matches("[1-3]") && b.matches("[1-3]")){
-                    aInt = Integer.parseInt(a);
-                    bInt = Integer.parseInt(b);
-                    System.out.println(aInt + " " + bInt);
-                    break;
-                }
-                else {
-                    System.out.println("Coordinates should be from 1 to 3!");
-                    System.out.println("Enter the coordinates: ");
+            while (true) {
+                if (a.matches("[0-9]+") && b.matches("[0-9]+")) {     //проверка на число
+                    if (a.matches("[1-3]") && b.matches("[1-3]")) {
+                        aInt = Integer.parseInt(a);
+                        bInt = Integer.parseInt(b);
+                        break;
+                    }
+                    else {
+                        System.out.println("Coordinates should be from 1 to 3!");
+                        System.out.print("Enter the coordinates: ");
+                        a = scanner.next();
+                        b = scanner.next();
+                    }
+                } else {
+                    System.out.println("You should enter numbers!");
+                    System.out.print("Enter the coordinates: ");
                     a = scanner.next();
                     b = scanner.next();
                 }
             }
+
+            if (arr[3 - bInt][aInt - 1] == '_') {
+                arr[3 - bInt][aInt - 1] = 'X';  //если ход Х
+                nX++;
+                break;
+            }
             else {
-                System.out.println("You should enter numbers!");
-                System.out.println("Enter the coordinates: ");
+                System.out.println("This cell is occupied! Choose another one!");
+                System.out.print("Enter the coordinates: ");
                 a = scanner.next();
                 b = scanner.next();
             }
         }
+        System.out.println("---------");     //печатаем поле
+        for (i = 0; i < 3; i++) {
+            System.out.print('|' + " ");
+            for (j = 0; j < 3; j++) {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println('|' + " ");
+        }
+        System.out.println("---------");
 
 
         if (nX - nO > 1 || nX - nO < -1) {   //ошибка когда Х или О больше чем на 1
             System.out.println("Impossible");
-        }
-        else {
+        } else {
             if ((arr[0][0] == arr[0][1] && arr[0][0] == arr[0][2])
-                || (arr[0][0] == arr[1][0] && arr[0][0] == arr[2][0])
-                || (arr[0][0] == arr[1][1] && arr[0][0] == arr[2][2])){
+                    || (arr[0][0] == arr[1][0] && arr[0][0] == arr[2][0])
+                    || (arr[0][0] == arr[1][1] && arr[0][0] == arr[2][2])) {
 
                 if (arr[0][0] == 'X' || arr[0][0] == 'O') {
                     win = arr[0][0];
@@ -86,7 +107,7 @@ public class Main {
                 }
             }
             if ((arr[1][0] == arr[1][1] && arr[1][0] == arr[1][2])
-                || (arr[0][1] == arr[1][1] && arr[0][1] == arr[2][1])){
+                    || (arr[0][1] == arr[1][1] && arr[0][1] == arr[2][1])) {
                 if (arr[1][1] == 'X' || arr[1][1] == 'O') {
                     win = arr[1][1];
                     res++;
@@ -99,27 +120,24 @@ public class Main {
                 }
             }
             if ((arr[0][2] == arr[1][2] && arr[0][2] == arr[2][2])
-                || (arr[0][2] == arr[1][1] && arr[0][2] == arr[2][0])){
+                    || (arr[0][2] == arr[1][1] && arr[0][2] == arr[2][0])) {
                 if (arr[0][2] == 'X' || arr[0][2] == 'O') {
                     win = arr[0][2];
                     res++;
                 }
             }
-            if(res >= 2){
+            if (res >= 2) {
                 System.out.println("Impossible");
-            }
-            else{
-                if(res == 1) {
+            } else {
+                if (res == 1) {
                     if (win == 'X' || win == 'O') {
                         System.out.println(win + " wins");
                     }
-                }
-                else{
-                    if(res == 0){
+                } else {
+                    if (res == 0) {
                         if (n > 0) {
                             System.out.println("Game not finished");
-                        }
-                        else {
+                        } else {
                             System.out.println("Draw");
                         }
                     }
